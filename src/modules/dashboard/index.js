6815ba8a25c144fc9ca7e4f795cc62cfd65d97c4
hemanth padala <hemanth330@gmail.com>
2020-11-24 19:46:23 -0500

    Initial commit

--- src/modules/dashboard/index.js
+++ src/modules/dashboard/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import {authRole} from '../../shared/constants/AppConst';
+import { authRole } from '../../shared/constants/AppConst';
 
 export const dashBoardConfigs = [
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/analytics',
-        component: React.lazy(() => import('./Analytics')),
-      },
-    ],
-  },
   {
     auth: authRole.user,
     routes: [
@@ -24,63 +15,9 @@ export const dashBoardConfigs = [
     auth: authRole.user,
     routes: [
       {
-        path: '/dashboards/health-care',
-        component: React.lazy(() => import('./HealthCare')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/academy',
-        component: React.lazy(() => import('./Academy')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/crm',
-        component: React.lazy(() => import('./CRM')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/crypto',
-        component: React.lazy(() => import('./Crypto')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/metrics',
-        component: React.lazy(() => import('./Metrics')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
-        path: '/dashboards/widgets',
-        component: React.lazy(() => import('./Widgets')),
-      },
-    ],
-  },
-  {
-    auth: authRole.user,
-    routes: [
-      {
         path: '/dashboards/profile',
         component: React.lazy(() => import('./Profile')),
       },
     ],
-  },
+  }
 ];
